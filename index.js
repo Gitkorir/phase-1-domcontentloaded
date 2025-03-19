fb177bcdda225b45d2a978edea414d5a158acf27
Arnold <arnoldkorir201@gmail.com>
2025-03-19 23:09:58 +0300

    use the .DOMContent Loaded event listener

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Your code goes here
+// Your code goes
+//function to change the text content
+document.addEventListener("DOMContentLoaded", function () {
+  const p = document.querySelector("p");
+  if (p) {
+    p.textContent = "This is really cool!";
+  }
+});
+// initial state befor the DOM is loaded
+document.addEventListener("DOMContentLoaded", function () {
+  const p = document.querySelector("p");
+  if (p) {
+    const newP = document.createElement("p");
+    newP.textContent =
+      "JavaScript is color.It lets me asss text to my page programme";
+    document.body.appendChild(newP);
+  }
+});
